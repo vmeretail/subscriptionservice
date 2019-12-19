31806faac0f0316a514f74e534fdf6404eb20281
Stuart Ferguson <stuart.ferguson@vmeretail.com>
2019-12-19 09:28:40 +0000

    Added Uri support on Subscription Value Object

--- src/SubscriptionService/Configuration/Subscription.cs
+++ src/SubscriptionService/Configuration/Subscription.cs
@@ -10,18 +10,23 @@
         #region Constructors
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Subscription"/> class.
+        /// Initializes a new instance of the <see cref="Subscription" /> class.
         /// </summary>
         /// <param name="streamName">Name of the stream.</param>
         /// <param name="groupName">Name of the group.</param>
-        /// <param name="endPointUrl">The end point URL.</param>
+        /// <param name="endPointUri">The end point URI.</param>
         private Subscription(String streamName,
                              String groupName,
-                             String endPointUrl)
+                             Uri endPointUri)
         {
+            if (endPointUri == null)
+            {
+                throw new ArgumentException("Value cannot be null", nameof(endPointUri));
+            }
+
             this.StreamName = streamName;
             this.GroupName = groupName;
-            this.EndPointUrl = endPointUrl;
+            this.EndPointUri = endPointUri;
         }
 
         #endregion
@@ -29,12 +34,12 @@
         #region Properties
 
         /// <summary>
-        /// Gets the end point URL.
+        /// Gets the end point Uri.
         /// </summary>
         /// <value>
-        /// The end point URL.
+        /// The end point Uri.
         /// </value>
-        public String EndPointUrl { get; }
+        public Uri EndPointUri { get; }
 
         /// <summary>
         /// Gets the name of the group.
@@ -67,7 +72,26 @@
                                           String groupName,
                                           String endPointUrl)
         {
-            return new Subscription(streamName, groupName, endPointUrl);
+            if (String.IsNullOrWhiteSpace(endPointUrl))
+            {
+                throw new ArgumentException("Value cannot be null or empty", nameof(endPointUrl));
+            }
+
+            return new Subscription(streamName, groupName, new Uri(endPointUrl));
+        }
+
+        /// <summary>
+        /// Creates the specified stream name.
+        /// </summary>
+        /// <param name="streamName">Name of the stream.</param>
+        /// <param name="groupName">Name of the group.</param>
+        /// <param name="endPointUri">The end point URI.</param>
+        /// <returns></returns>
+        public static Subscription Create(String streamName,
+                                          String groupName,
+                                          Uri endPointUri)
+        {
+            return new Subscription(streamName, groupName, endPointUri);
         }
 
         #endregion
---
+++ test/SubscriptionService.UnitTests/SubscriptionTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubscriptionService.UnitTests
+{
+    using Configuration;
+    using Shouldly;
+    using Xunit;
+
+    public class SubscriptionTests
+    {
+        [Fact]
+        public void Subscription_EndpointAsString_SubscriptionCreated()
+        {
+            // 1. Arrange
+            String endpointUrl = "http://127.0.0.1:2113/";
+            String streamName = "TestStream";
+            String groupName = "GroupName";
+            
+            // 2. Act
+            Subscription subscription = Subscription.Create(streamName, groupName, endpointUrl);
+
+            // 3. Assert
+            subscription.ShouldNotBeNull();
+            subscription.EndPointUri.AbsoluteUri.ShouldBe(endpointUrl);
+            subscription.GroupName.ShouldBe(groupName);
+            subscription.StreamName.ShouldBe(streamName);
+        }
+
+        [Fact]
+        public void Subscription_EndpointAsUrl_SubscriptionCreated()
+        {
+            // 1. Arrange
+            Uri endpointUri = new Uri("http://127.0.0.1:2113/");
+            String streamName = "TestStream";
+            String groupName = "GroupName";
+
+            // 2. Act
+            Subscription subscription = Subscription.Create(streamName, groupName, endpointUri);
+
+            // 3. Assert
+            subscription.ShouldNotBeNull();
+            subscription.EndPointUri.Equals(endpointUri).ShouldBeTrue();
+            subscription.GroupName.ShouldBe(groupName);
+            subscription.StreamName.ShouldBe(streamName);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Subscription_EndpointStringInvalidValue_ErrorIsThrown(String endpointUrl)
+        {
+            // 1. Arrange
+            String streamName = "TestStream";
+            String groupName = "GroupName";
+
+            // 3. Assert
+            Should.Throw<ArgumentException>(() => Subscription.Create(streamName, groupName, endpointUrl));
+        }
+
+        [Fact]
+        public void Subscription_EndpointUriInvalidValue_ErrorIsThrown()
+        {
+            // 1. Arrange
+            Uri endpointUri = null;
+            String streamName = "TestStream";
+            String groupName = "GroupName";
+
+            // 3. Assert
+            Should.Throw<ArgumentException>(() => Subscription.Create(streamName, groupName, endpointUri));
+        }
+    }
+}
