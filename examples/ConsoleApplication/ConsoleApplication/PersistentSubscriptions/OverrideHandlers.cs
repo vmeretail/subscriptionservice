fa2975bbda513f071154ee67d1e3144ae74c2cfb
Steven Blair <steven.blair@vmeretail.com>
2020-03-31 10:34:19 +0100

    Update console comments and added a couple more examples.

---
+++ examples/ConsoleApplication/ConsoleApplication/PersistentSubscriptions/OverrideHandlers.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApplication.PersistentSubscriptions
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using EventStore.ClientAPI;
+    using SubscriptionService;
+    using SubscriptionService.Builders;
+    using SubscriptionService.Extensions;
+
+    public class OverrideHandlers
+    {
+        public static async Task AllHandlers(IEventStoreConnection eventStoreConnection)
+        {
+            Uri uri = new Uri("https://ennxdwa7hkx8e.x.pipedream.net/");
+
+            PersistentSubscriptionBuilder builder = PersistentSubscriptionBuilder.Create("$ce-PersistentTest", "Persistent Test 1")
+                                                                                 .UseConnection(eventStoreConnection)
+                                                                                 .AddEventAppearedHandler((@base,
+                                                                                                           @event) =>
+                                                                                                          {
+                                                                                                              Console.WriteLine("Override EventAppeared called.");
+                                                                                                          })
+                                                                                 .AddSubscriptionDroppedHandler((uribase,
+                                                                                                                 reason,
+                                                                                                                 arg3) =>
+                                                                                                                {
+                                                                                                                    Console.WriteLine("SubscriptionDropped override.");
+                                                                                                                })
+                                                                                 .DeliverTo(uri);
+
+            Subscription subscription = builder.Build();
+
+            await subscription.Start(CancellationToken.None);
+        }
+    }
+}
